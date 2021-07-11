408e4efc76ebb74629ec836a3df399de932e7e91
stickmd <stickmd@gmail.com>
2021-07-11 04:31:57 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # tempclc
 Command-line utility for temperature conversion
+
+I was basically bored when developing this and just wanted to test out more advanced input parsing in C++. Got something suprisingly useful in result.
+
+# Compilation and installation
+Classically:
+
+```
+$ make
+$ sudo make install
+```
+
+# Usage
+
+```
+Usage: tempclc [-uv] value<c|C|f|F>
+
+-u, --units    Show units in output
+-v, --verbose  Show output as XX = YY
+-h, --help     Print this help message and exit
+```
+
+# Examples
+
+Convert 32 Fahrenheit to Celcius:
+
+```
+$ tempclc 32F
+0
+```
+
+Convert 100 Celcius to Fahrenheir and display the unit in output:
+
+```
+$ tempclc -u 100C
+212°F
+```
+
+Convert 59 Fahrenheit to Celcius, include input in the output and present it with units:
+```
+$ $ tempclc -u -v 59F
+59°F = 15°C
+```
+
+$ Credits
+
+- [Simon Schneegans](http://schneegans.github.io/) for his outstanding lightweight [parsing class](http://schneegans.github.io/tutorials/2019/08/06/commandline)
---
+++ src/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include "CommandLine.hpp"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    // Default options
+    bool    oUnits      = false;
+    bool    oVerbose    = false;
+    bool    oPrintHelp  = false;
+
+    // Declaration of all possible command line options
+    CommandLine args("Usage: tempclc [-uv] value<c|C|f|F>");
+    args.addArgument({"-u", "--units"},     &oUnits,    "Show units in output");
+    args.addArgument({"-v", "--verbose"},   &oVerbose,  "Show output as XX = YY");
+    args.addArgument({"-h", "--help"},      &oPrintHelp,
+            "Print this help message and exit");
+
+    // Parse command line arguments
+    try
+    {
+        args.parse(argc, argv);
+    }
+    catch (std::runtime_error const& e)
+    {
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
+    // If oPrintHelp was set to true, print a help message and exit.
+    if (oPrintHelp)
+    {
+        args.printHelp();
+        return 0;
+    }
+    
+    // Find the temperature value in passed arguments
+    string input;
+    float v = 0;
+    for (int i = 1; i < argc; i++)
+    {
+        input = argv[i];
+        if ((input.find('c') != string::npos) || (input.find('C') != string::npos))
+        {
+            if (input.find('c') != string::npos) v = stof(input.substr(0,input.find('c')));
+            if (input.find('C') != string::npos) v = stof(input.substr(0,input.find('C')));
+            if (oVerbose)
+            {
+                float org = v;
+                v = (1.8 * v) + 32;
+                if (oUnits) cout << org << "°C = " << v << "°F" << endl;
+                else cout << org << " = " << v << endl;
+                return 0;
+            }
+            else
+            {
+                v = (1.8 * v) + 32;
+                if (oUnits) cout << v << "°F" << endl;
+                else cout << v << endl;
+                return 0;
+            }
+        }
+        else if ((input.find('f') != string::npos) || (input.find('F') != string::npos))
+        {
+            if (input.find('f') != string::npos) v = stof(input.substr(0,input.find('f')));
+            if (input.find('F') != string::npos) v = stof(input.substr(0,input.find('F')));
+            if (oVerbose)
+            {
+                float org = v;
+                v = (v - 32) / 1.8;
+                if (oUnits) cout << org << "°F = " << v << "°C" << endl;
+                else cout << org << " = " << v << endl;
+                return 0;
+            }
+            else
+            {
+                v = (v - 32) / 1.8;
+                if (oUnits) cout << v << "°C" << endl;
+                else cout << v << endl;
+                return 0;
+            }
+        }
+    }
+    
+    cout << "[ERROR] No value found" << endl;
+    return 2;
+}
